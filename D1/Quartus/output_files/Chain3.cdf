b19eaf870e535258ffeb70023e6ce03a118102a9
Joseph Butterworth (jdb1g20) <jdb1g20@soton.ac.uk>
2021-11-15 11:45:34 +0000

    D1 FIR Parametrised

---
+++ D1/Quartus/output_files/Chain3.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 16.1.2 Build 203 01/18/2017 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("H:/D1/SystemVerilog-Programming-II/D1/Quartus/output_files/") File("D1.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- D1/fir.sv
+++ D1/fir.sv
@@ -1,25 +1,28 @@
-// FIR 16 stages; 16 bit samples. Could be parameterised.
+// FIR N stages; M bit samples.
 
-module fir (input logic signed [15:0] in,
-       input logic input_ready, ck, rst ,
-       output logic signed [15:0] out,
-       output logic output_ready);
+module fir #(parameter N=16; parameter M=24;)
+		(input logic signed [M-1:0] in,
+		input logic input_ready, ck, rst, 
+		output logic signed [M-1:0] out,
+		output logic output_ready);
 
-typedef logic signed [15:0] sample_array;
-sample_array samples [0:15];
+typedef logic signed [M-1:0] sample_array;
+sample_array samples [0:N-1];
 
 // generate coefficients from Octave/Matlab
 // disp(sprintf('%d,',round(fir1(15,0.5)*32768)))
+// https://verificationguide.com/systemverilog/systemverilog-dpi/#Import_Method
+// https://stackoverflow.com/questions/61995984/matlab-script-use-in-system-verilog-using-snps-vcs-tool
 
-const sample_array coefficients [0:15] =
+const sample_array coefficients [0:N-1] =
      '{-81, -134,  318, 645,
      -1257, -2262, 4522, 14633,
      14633, 4522, -2262,-1257,
      645, 318, -134, -81};
 
-logic unsigned [3:0] address; //clog2 of 16 is 4
+logic unsigned [$clog2(N)-1:0] address; //clog2 of 16 is 4
 
-logic signed [31:0] sum;
+logic signed [2*N-1:0] sum;
 
 typedef enum logic [1:0] {waiting, loading, processing, saving} state_type;
 state_type state, next_state;
@@ -29,7 +32,7 @@ logic load, count, reset_accumulator;
 always_ff @(posedge ck)
   if (load)
     begin
-    for (int i=15; i >= 1; i--)
+    for (int i=N-1; i >= 1; i--)
       samples[i] <= samples[i-1];
     samples[0] <= in;
     end
@@ -44,7 +47,7 @@ always_ff @(posedge ck)
     
 always_ff @(posedge ck)
   if (output_ready)
-    out <= sum[30:15];
+    out <= sum[2*(M-1):M-1];
     
 // address counter
 
@@ -93,7 +96,7 @@ always_comb
             end
             processing : begin
                 count = '1;                 //Assert unconditional output count
-                if (address == 15)          //Conditional to progress to saving
+                if (address == N-1)         //Conditional to progress to saving
                     next_state = saving;
                 else                        //Else increment address again
                     next_state = processing;
@@ -107,5 +110,3 @@ always_comb
         endcase
     end
 endmodule
-
-
